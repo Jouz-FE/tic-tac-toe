462e741e4e45bf43c9d20c00921197d4ba274ae4
Jouz-FE <fr2d2ric.ma11e@gmail.com>
2020-06-15 01:45:52 +0900

    Updates

---
+++ precache-manifest.491cae7f4109f520be7b185e93fddeee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f39961a00c4ebed2c7bba9f9c8da2af",
+    "url": "/Jouz-FE.github.io/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "72cce8b3e6f643f77485",
+    "url": "/Jouz-FE.github.io/tic-tac-toe/static/css/main.374da615.chunk.css"
+  },
+  {
+    "revision": "4c1af65e46252d7cd254",
+    "url": "/Jouz-FE.github.io/tic-tac-toe/static/js/2.be9f4df5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jouz-FE.github.io/tic-tac-toe/static/js/2.be9f4df5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72cce8b3e6f643f77485",
+    "url": "/Jouz-FE.github.io/tic-tac-toe/static/js/main.10074a89.chunk.js"
+  },
+  {
+    "revision": "c668d7bf26c831964a19",
+    "url": "/Jouz-FE.github.io/tic-tac-toe/static/js/runtime-main.12b61494.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Jouz-FE.github.io/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
